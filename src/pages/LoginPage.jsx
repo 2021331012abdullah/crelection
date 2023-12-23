fdccc1e68fb1b49b5e6c769ebc87977b39026441
Abdullah Al Mahmud <138648851+2021331012abdullah@users.noreply.github.com>
2023-12-23 11:18:02 +0600

    Add files via upload

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -73,7 +73,7 @@ const LoginPage = () => {
 
                 </div>
             </form>
-
+            <p>Dont have an account? Register <Link to="/register">here</Link></p>
             
           </div>
       </div>

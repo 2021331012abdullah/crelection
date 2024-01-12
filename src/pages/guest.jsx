4b931881216b3a8699aae26b4fca412d1c54acaa
Abdullah Al Mahmud <138648851+2021331012abdullah@users.noreply.github.com>
2024-01-12 22:31:18 +0600

    Update guest.jsx

--- src/pages/guest.jsx
+++ src/pages/guest.jsx
@@ -86,7 +86,7 @@ const Guest = () => {
                     <br/>
                     </div >
                     <br/>
-                    <div className='center-align'>Final result will be announced after 10PM</div>
+                    <div className='center-align'>Result in published!</div>
                     
                     
                     <br/>

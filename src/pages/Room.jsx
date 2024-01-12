077b1fb2de5d40747d5cb1f4d51fcba2bac63a29
Abdullah Al Mahmud <138648851+2021331012abdullah@users.noreply.github.com>
2024-01-12 21:20:28 +0600

    Update Room.jsx

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -126,7 +126,7 @@ const Room = () => {
             {voted===1 ?
             (<>
                 <div className='message--header'>
-                    <h2>আগে ছবি তে ক্লিক করে তারপর ভোট দিন নিচের বাটন চাপলে আর পরিবর্তন করার সুযোগ থাকবে না</h2>
+
                     <br></br>
                     <div className='center-align'><img src={udayImage} width={sz} height={sz} alt="uday" /><h2>Uday</h2></div>
                     
@@ -143,6 +143,7 @@ const Room = () => {
                 <br />
                 <p>✅ আপনি ইতোমধ্যে একবার ভোট দিয়েছেন</p></>): voted===0?
         (<div> <div className='message--header'>
+                <h2>আগে ছবি তে ক্লিক করে তারপর ভোট দিন নিচের বাটন চাপলে আর পরিবর্তন করার সুযোগ থাকবে না</h2>
             {/* The first radio button is checked by default using the defaultChecked attribute */}
             <label><input type='radio' name="favourite" value="uday" onChange={()=>{setChoice("uday"); }}></input><img src={udayImage} width={sz} height={sz} alt="uday" /></label>
             <label><input type='radio' name="favourite" value="tanzim" defaultChecked onChange={()=>{setChoice("tanzim");}}></input><img src={tanzimImage} width={sz} height={sz} alt="tanzim" /></label><br/>
